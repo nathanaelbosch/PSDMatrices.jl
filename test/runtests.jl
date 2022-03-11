5fb19d22921d563e79baa8e25959296e23e14030
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2022-03-11 14:35:47 +0100

    Wrap the tests in a single big testset to always run them all
    And it makes the printing a bit more pretty

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,33 +5,35 @@ using LinearAlgebra
 Mbase = [1 1; 2 20]
 eltypes = (Int64, Float64, BigFloat)
 
-@testset "eltype=$t" for t in eltypes
-    M = t.(Mbase)
-    S = PSDMatrix(M)
+@testset "PSDMatrices.jl" begin
+    @testset "eltype=$t" for t in eltypes
+        M = t.(Mbase)
+        S = PSDMatrix(M)
 
-    @test eltype(S) == eltype(M) == t
+        @test eltype(S) == eltype(M) == t
 
-    @testset "My exports" begin
-        @test norm(todense(S) - M * M') == 0.0
-        @test todense(X_A_Xt(S, M)) ≈ M * todense(S) * M'
-        @test choleskify_factor(S).L ≈ cholesky(todense(S)).U'
-        @test todense(add_cholesky(S, S)) ≈ todense(S) + todense(S)
-        @test todense(add_qr(S, S)) ≈ todense(S) + todense(S)
-    end
+        @testset "My exports" begin
+            @test norm(todense(S) - M * M') == 0.0
+            @test todense(X_A_Xt(S, M)) ≈ M * todense(S) * M'
+            @test choleskify_factor(S).L ≈ cholesky(todense(S)).U'
+            @test todense(add_cholesky(S, S)) ≈ todense(S) + todense(S)
+            @test todense(add_qr(S, S)) ≈ todense(S) + todense(S)
+        end
 
-    @testset "Base" begin
-        @test todense(inv(S)) ≈ inv(todense(S))
-        @test size(S) == size(todense(S))
-        @test S \ M ≈ todense(S) \ M
-        @test M / S ≈ M / todense(S)
-        @test S == S
-        @test copy(S) == S
-        @test !(copy(S) === S)
-    end
+        @testset "Base" begin
+            @test todense(inv(S)) ≈ inv(todense(S))
+            @test size(S) == size(todense(S))
+            @test S \ M ≈ todense(S) \ M
+            @test M / S ≈ M / todense(S)
+            @test S == S
+            @test copy(S) == S
+            @test !(copy(S) === S)
+        end
 
-    @testset "LinearAlgebra" begin
-        @test det(S) ≈ det(todense(S))
-        @test logdet(S) ≈ logdet(todense(S))
+        @testset "LinearAlgebra" begin
+            @test det(S) ≈ det(todense(S))
+            @test logdet(S) ≈ logdet(todense(S))
+        end
     end
 end
 nothing
