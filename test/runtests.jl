fa04e4e5900e9153697ca0e0aa1d17f115e8a440
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2022-03-11 14:56:44 +0100

    Refactor the tests to use a single matrix and cast them to different types (#11)
    * Use the same matrix and cast it to the different types
    
    * Wrap the tests in a single big testset to always run them all
    
    And it makes the printing a bit more pretty
    
    * Update test/runtests.jl
    
    Co-authored-by: Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
    
    Co-authored-by: Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,36 +2,38 @@ using Test
 using PSDMatrices
 using LinearAlgebra
 
-M1 = [1 1; 2 20]
-M2 = [1.0 1.0; 2.0 20.0]
-M3 = big.([1.0 1.0; 2.0 20.0])
+Mbase = [1 1; 2 20]
+eltypes = (Int64, Float64, BigFloat)
 
-@testset "$M" for M in (M1, M2, M3)
-    S = PSDMatrix(M)
+@testset "PSDMatrices.jl" begin
+    @testset "eltype=$t" for t in eltypes
+        M = t.(Mbase)
+        S = PSDMatrix(M)
 
-    @test eltype(S) == eltype(M)
+        @test eltype(S) == t
 
-    @testset "My exports" begin
-        @test norm(todense(S) - M * M') == 0.0
-        @test todense(X_A_Xt(S, M)) ≈ M * todense(S) * M'
-        @test choleskify_factor(S).L ≈ cholesky(todense(S)).U'
-        @test todense(add_cholesky(S, S)) ≈ todense(S) + todense(S)
-        @test todense(add_qr(S, S)) ≈ todense(S) + todense(S)
-    end
+        @testset "My exports" begin
+            @test norm(todense(S) - M * M') == 0.0
+            @test todense(X_A_Xt(S, M)) ≈ M * todense(S) * M'
+            @test choleskify_factor(S).L ≈ cholesky(todense(S)).U'
+            @test todense(add_cholesky(S, S)) ≈ todense(S) + todense(S)
+            @test todense(add_qr(S, S)) ≈ todense(S) + todense(S)
+        end
 
-    @testset "Base" begin
-        @test todense(inv(S)) ≈ inv(todense(S))
-        @test size(S) == size(todense(S))
-        @test S \ M ≈ todense(S) \ M
-        @test M / S ≈ M / todense(S)
-        @test S == S
-        @test copy(S) == S
-        @test !(copy(S) === S)
-    end
+        @testset "Base" begin
+            @test todense(inv(S)) ≈ inv(todense(S))
+            @test size(S) == size(todense(S))
+            @test S \ M ≈ todense(S) \ M
+            @test M / S ≈ M / todense(S)
+            @test S == S
+            @test copy(S) == S
+            @test !(copy(S) === S)
+        end
 
-    @testset "LinearAlgebra" begin
-        @test det(S) ≈ det(todense(S))
-        @test logdet(S) ≈ logdet(todense(S))
+        @testset "LinearAlgebra" begin
+            @test det(S) ≈ det(todense(S))
+            @test logdet(S) ≈ logdet(todense(S))
+        end
     end
 end
 nothing
