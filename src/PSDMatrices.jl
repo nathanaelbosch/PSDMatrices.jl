bcd0c704d3e5a7af108ab2324425862a4afd39ba
Nicholas Krämer <nicokraemer@gmx.net>
2022-03-11 12:22:27 +0100

    PSD matrices refurnished (#2)
    * moved over SquareRootmatrices code
    
    * SquareRootMatrix -> PSDMatrix
    
    * Updated readme
    
    * A -> L in README
    
    * Cholesky -> Choleskify
    
    * Organise tests
    
    * fix imports
    
    * Moved custom functions to bottom of file
    
    * Delete empty file

--- src/PSDMatrices.jl
+++ src/PSDMatrices.jl
@@ -1,13 +1,64 @@
+
 module PSDMatrices
 
+import Base: \, /, size, inv, copy, ==
 using LinearAlgebra
-import LinearAlgebra: cholesky, diag, det, logdet
+import LinearAlgebra: det, logdet
 
-import Base: +, *, /, \, ==, inv, Matrix, kron, copy
+struct PSDMatrix{T,Ltype} <: AbstractMatrix{T}
+    L::Ltype
+    PSDMatrix(L::AbstractMatrix{T}) where {T} = new{T,typeof(L)}(L)
+end
 
-# Write your package code here.
-include("psdmatrix.jl")
+
+# Base overloads
+
+size(M::PSDMatrix) = (size(M.L, 1), size(M.L, 1))
+inv(M::PSDMatrix) = PSDMatrix(inv(M.L'))
+\(A::PSDMatrix, B::AbstractVecOrMat) = A.L' \ (A.L \ B)
+/(B::AbstractVecOrMat, A::PSDMatrix) = B / A.L' / A.L
+copy(M::PSDMatrix) = PSDMatrix(copy(M.L))
+==(M1::PSDMatrix, M2::PSDMatrix) = M1.L == M2.L  # todo: same as isequal()?!
+
+
+
+# LinearAlgebra overloads
+
+det(M::PSDMatrix) = det(M.L)^2
+logdet(M::PSDMatrix) = 2 * logdet(M.L)
+
+
+function nonnegative_diagonal(R)
+    signs = signbit.(diag(R))
+    R .*= (1 .- 2 .* signs)
+    return R
+end
+
+
+
+
+# Custom functions
+
+function todense(M::PSDMatrix)
+    return M.L * M.L'
+end
+
+
+function X_A_Xt(A::PSDMatrix, X::AbstractMatrix)
+    return PSDMatrix(X * A.L)
+end
+
+
+function choleskify_factor(M::PSDMatrix)
+    QR = qr(M.L')
+    R = triu(QR.factors)
+    R = nonnegative_diagonal(R)
+    return PSDMatrix(LowerTriangular(R'))
+end
 
 export PSDMatrix
+export todense
+export choleskify_factor
+export X_A_Xt
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,71 +1,41 @@
-using PSDMatrices
 using Test
-
-using PSDMatrices: X_A_Xt
+using PSDMatrices
 using LinearAlgebra
 
-@testset "PSDMatrices.jl" begin
 
-    d = 5
-    L = LowerTriangular(randn(d,d))
-    LLT = L*L'
-    A = PSDMatrix(L)
-    B = PSDMatrix(LowerTriangular(rand(d,d)))
 
-    @testset "Basics" begin
-        @test A == LLT
-        @test all(A .== LLT)
-        @test size(A) == size(LLT)
-        @test A[:] == LLT[:]
+M1 = [1 1; 2 20]
+M2 = [1.0 1.0; 2.0 20.0]
+M3 = big.([1.0 1.0; 2.0 20.0])
 
-        @test PSDMatrix(Matrix(A)) ≈ A
 
-        @test copy(A) isa PSDMatrix
-        copy!(B, A)
-        @test B == A
+@testset "$M" for M in (M1, M2, M3)
 
-        @test Matrix(A) == LLT
-        @test typeof(Matrix(A)) === typeof(LLT)
+    S = PSDMatrix(M)
+
+    @test eltype(S) == eltype(M)
+
+    @testset "My exports" begin
+        @test norm(todense(S) - M * M') == 0.0
+        @test todense(X_A_Xt(S, M)) ≈ M * todense(S) * M'
+        @test choleskify_factor(S).L ≈ cholesky(todense(S)).U'
+    end
+
+    @testset "Base" begin
+        @test todense(inv(S)) ≈ inv(todense(S))
+        @test size(S) == size(todense(S))
+        @test S \ M ≈ todense(S) \ M
+        @test M / S ≈ M / todense(S)
+        @test S == S
+        @test copy(S) == S
+        @test !(copy(S) === S)
     end
 
     @testset "LinearAlgebra" begin
-        @test diag(A) == diag(LLT)
-        @test det(A) ≈ det(LLT)
-        @test logdet(A) ≈ logdet(LLT)
-
-        @test inv(A) ≈ inv(LLT)
-        x = rand(d)
-        @test A\x ≈ LLT\x
-
-        @test cholesky(A) == cholesky(LLT)
+        @test det(S) ≈ det(todense(S))
+        @test logdet(S) ≈ logdet(todense(S))
     end
 
-    @testset "X_A_Xt" begin
-        X = rand(d,d)
-        @test X_A_Xt(A, X) isa PSDMatrix
-        @test X_A_Xt(A, X) ≈ X*A*X'
 
-        D = Diagonal(rand(d))
-        @test X_A_Xt(A, D) isa PSDMatrix
-        @test X_A_Xt(A, D) ≈ D*A*D'
-    end
-
-    @testset "Arithmetic" begin
-        L2 = LowerTriangular(randn(d,d))
-        B = PSDMatrix(L2)
-        LLT2 = L2*L2'
-        @test A+B isa PSDMatrix
-        @test A+B ≈ LLT+LLT2
-
-        c = rand()
-        @test c*A isa PSDMatrix
-        @test 0*A isa PSDMatrix
-        @test !(-1*A isa PSDMatrix)
-        @test c*A == A*c
-    end
-
-    @testset "BigFloats" begin
-        L = LowerTriangular(big.(randn(d,d)))
-        @test PSDMatrix(L) isa PSDMatrix
-    end
 end
+nothing
