3a6dc5ec89b51d8b9476953e10d468ca0bd41465
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2022-03-13 12:07:49 +0100

    Exclude the `copy!` test for BigFloats to be 1.0 compatible

--- test/runtests.jl
+++ test/runtests.jl
@@ -22,7 +22,7 @@ eltypes = (Int64, Float64, BigFloat)
             @test copy(S) == S
             @test !(copy(S) === S)
             @test typeof(similar(S)) == typeof(S)
-            @test (S2 = similar(S); copy!(S2, S); S2 == S)
+            (t != BigFloat) && @test (S2 = similar(S); copy!(S2, S); S2 == S)
             if size(M, 1) == size(M, 2)
                 @test todense(inv(S)) ≈ inv(todense(S))
             end
