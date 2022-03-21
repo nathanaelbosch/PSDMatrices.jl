f5ecc8139ec71b4eb06f3a92a37f0bcd82a19b39
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2022-03-21 14:46:35 +0100

    Remove unused clutter from the `similar` implementation

--- src/PSDMatrices.jl
+++ src/PSDMatrices.jl
@@ -17,8 +17,7 @@ inv(M::PSDMatrix) = PSDMatrix(inv(M.R'))
 \(A::PSDMatrix, B::AbstractVecOrMat) = A.R \ (A.R' \ B)
 /(B::AbstractVecOrMat, A::PSDMatrix) = B / A.R / A.R'
 copy(M::PSDMatrix) = PSDMatrix(copy(M.R))
-similar(M::PSDMatrix, element_type::Type{T}=eltype(M)) where {T,N} =
-    PSDMatrix(similar(M.R, element_type))
+similar(M::PSDMatrix, element_type::Type=eltype(M)) = PSDMatrix(similar(M.R, element_type))
 copy!(dst::PSDMatrix, src::PSDMatrix) = (copy!(dst.R, src.R); dst)
 ==(M1::PSDMatrix, M2::PSDMatrix) = M1.R == M2.R  # todo: same as isequal()?!
 function show(io::IO, M::PSDMatrix)
