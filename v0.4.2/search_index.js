26029bd6e8172565ddb55378ee37ac8d558f368a
Documenter.jl <documenter@juliadocs.github.io>
2022-03-23 12:56:30 +0000

    build based on 067e737

---
+++ v0.4.2/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PSDMatrices","category":"page"},{"location":"#PSDMatrices","page":"Home","title":"PSDMatrices","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PSDMatrices]","category":"page"}]
+}
