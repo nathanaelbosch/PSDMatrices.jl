5f058eec005ec91e615c0e8ff3144df338795154
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2023-11-23 19:06:56 +0100

    Make PSDMatrices <: AbstractPDMat to use them with Distributions.jl

--- src/PSDMatrices.jl
+++ src/PSDMatrices.jl
@@ -5,7 +5,11 @@ import Base: \, /, size, inv, copy, copy!, ==, show, similar, Matrix, iszero, is
 using LinearAlgebra
 import LinearAlgebra: det, logabsdet, diag
 
-struct PSDMatrix{T,FactorType} <: AbstractMatrix{T}
+using PDMats
+using PDMats
+import PDMats: unwhiten!
+
+struct PSDMatrix{T,FactorType} <: AbstractPDMat{T}
     R::FactorType
 end
 PSDMatrix(R::AbstractMatrix{T}) where {T} = PSDMatrix{T,typeof(R)}(R)
@@ -15,6 +19,7 @@ PSDMatrix{T}(R::AbstractMatrix) where {T} = PSDMatrix{T,typeof(R)}(R)
 Matrix(M::PSDMatrix) = Matrix(M.R' * M.R)
 unfactorize(M::PSDMatrix) = M.R' * M.R
 size(M::PSDMatrix) = (size(M.R, 2), size(M.R, 2))
+ndims(M::PSDMatrix) = 2
 inv(M::PSDMatrix) = PSDMatrix(inv(M.R'))
 iszero(M::PSDMatrix) = iszero(M.R)
 \(A::PSDMatrix, B::AbstractVecOrMat) = A.R \ (A.R' \ B)
@@ -65,19 +70,48 @@ function confirm_factor_is_square(M::PSDMatrix)
             "The factor of the received PSDMatrix has dimensions ($(size(M.R,1)), $(size(M.R,2))). " *
             "Try turning the PSDMatrix into a dense matrix first."
         )
-        throw(MethodError(msg))
+        error(msg)
     end
 end
 
-# Custom functions
+# PDMats.jl overloads
+choleskify(M::PSDMatrix) = Cholesky(nonnegative_diagonal!(qr(M.R).R), 'U', 0)
+function nonnegative_diagonal!(R)
+    signs = signbit.(diag(R))
+    R .*= (1 .- 2 .* signs)
+    return R
+end
+function PDMats.unwhiten!(r::AbstractVecOrMat, M::PSDMatrix, x::AbstractVecOrMat)
+    PDMats.@check_argdims axes(r) == axes(x)
+    PDMats.@check_argdims size(M.R, 2) == size(x, 1)
+    return mul!(r, choleskify(M).L, x)
+end
+function PDMats.unwhiten(M::PSDMatrix, x::AbstractVecOrMat)
+    PDMats.@check_argdims size(M.R, 1) == size(x, 1)
+    return choleskify(M).L * x
+end
+function PDMats.whiten!(r::AbstractVecOrMat, M::PSDMatrix, x::AbstractVecOrMat)
+    PDMats.@check_argdims axes(r) == axes(x)
+    PDMats.@check_argdims size(M.R, 2) == size(x, 1)
+    return ldiv!(r, choleskify(M).L, x)
+end
+function PDMats.whiten(M::PSDMatrix, x::AbstractVecOrMat)
+    PDMats.@check_argdims size(M.R, 1) == size(x, 1)
+    return choleskify(M).L \ x
+end
 
-X_A_Xt(; A::PSDMatrix, X::AbstractMatrix) = PSDMatrix(A.R * X')
-X_A_Xt(A::PSDMatrix, X::AbstractMatrix) = X_A_Xt(A=A, X=X)
+PDMats.X_A_Xt(A::PSDMatrix, X::AbstractVecOrMat) = PSDMatrix(A.R * X')
+PDMats.Xt_A_X(A::PSDMatrix, X::AbstractVecOrMat) = PSDMatrix(A.R * X)
+PDMats.X_invA_Xt(A::PSDMatrix, X::AbstractVecOrMat) = PSDMatrix(A.R' \ X')
+PDMats.Xt_invA_X(A::PSDMatrix, X::AbstractVecOrMat) = PSDMatrix(A.R' \ X)
+
+
 function X_A_Xt!(out::PSDMatrix; A::PSDMatrix, X::AbstractMatrix)
     mul!(out.R, A.R, X')
     return out
 end
 X_A_Xt!(out::PSDMatrix, A::PSDMatrix, X::AbstractMatrix) = X_A_Xt!(out, A=A, X=X)
+
 
 function add_cholesky(A::PSDMatrix, B::PSDMatrix)
     sum_dense = Matrix(A) + Matrix(B)
@@ -100,7 +134,7 @@ export PSDMatrix
 export add_cholesky
 export add_qr
 export triangularize_factor
-export X_A_Xt
+# export X_A_Xt
 export X_A_Xt!
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,20 +2,24 @@ using Test
 using PSDMatrices
 using LinearAlgebra
 using Suppressor
-using Aqua
+using Aqua, JET
+using PDMats
 
 M_square = [1 1; 2 20]
 M_tall = [1 1; 2 20; 3 30]
 M_wide = [1 1 1; 2 20 200]
 M_neg = -ones(1, 1)
-eltypes = (Int64, Float64, BigFloat)
+# eltypes = (Int64, Float64, BigFloat)
+eltypes = (Float64, )
+# sizes = (M_square, M_tall, M_wide, M_neg)
+sizes = (M_square,)
 
 @testset "PSDMatrices.jl" begin
-    @testset "eltype=$t | shape=$(size(Mbase))" for t in eltypes,
-        Mbase in (M_square, M_tall, M_wide, M_neg)
+    @testset "eltype=$t | shape=$(size(Mbase))" for t in eltypes, Mbase in sizes
 
         M = t.(Mbase)
         S = PSDMatrix(M)
+        SM = M'M
         X = rand(t, size(S, 1), size(S, 2))
 
         @testset "Base" begin
@@ -57,7 +61,6 @@ eltypes = (Int64, Float64, BigFloat)
         @testset "Exports" begin
             @test norm(Matrix(S) - M' * M) == 0.0
             @test Matrix(X_A_Xt(S, X)) ≈ X * Matrix(S) * X'
-            @test Matrix(X_A_Xt(A=S, X=X)) ≈ X * Matrix(S) * X'
             @test begin
                 product_eltype = typeof(one(eltype(X)) * one(eltype(S)))
                 S2 = PSDMatrix(zeros(product_eltype, size(S.R)...))
@@ -78,9 +81,60 @@ eltypes = (Int64, Float64, BigFloat)
                 @test Matrix(tri) ≈ Matrix(S)
             end
         end
+
+        if size(S.R, 1) == size(S.R, 2)
+        @testset "PDMats.jl interface" begin
+            i = 1
+            x = rand(t, size(S, 2), size(S, 2))
+            r = similar(x)
+            c = rand(t)
+            @test size(S) == size(SM)
+            @test size(S, i) == size(SM, i)
+            @test ndims(S) == ndims(SM)
+            @test_nowarn eltype(S)
+            @test_nowarn Matrix(S)
+            @test diag(S) ≈ diag(SM)
+            @test Matrix(inv(S)) ≈ inv(SM)
+            @test_broken eigmax(S)
+            @test_broken eigmin(S)
+            @test logdet(S) ≈ logdet(SM)
+            @test_broken S * x
+            @test S \ x ≈ SM \ x
+            @test_broken S * c
+            @test_broken c * S
+            @test_broken S + b
+            # @test_nowarn pdadd(S, b, c)
+            # @test_nowarn pdadd(m, S)
+            # @test_nowarn pdadd(m, S, c)
+            # @test_nowarn pdadd!(m, S)
+            # @test_nowarn pdadd!(m, S, c)
+            # @test_nowarn pdadd!(r, m, S)
+            # @test_nowarn pdadd!(r, m, S, c)
+            # @test_nowarn quad(S, x)
+            # @test_nowarn quad!(copy(S), S, x)
+            # @test_nowarn invquad(S, x)
+            # @test_nowarn invquad!(r, S, x)
+            @test Matrix(X_A_Xt(S, x')) ≈ x' * SM * x
+            @test Matrix(Xt_A_X(S, x)) ≈ x' * SM * x
+            @test Matrix(X_invA_Xt(S, x')) ≈ x' * inv(SM) * x
+            @test Matrix(Xt_invA_X(S, x)) ≈ x' * inv(SM) * x
+            @test whiten(S, x) ≈ whiten(SM, x)
+            @test whiten!(S, copy(x)) ≈ whiten(SM, x)
+            @test whiten!(r, S, x) ≈ whiten(SM, x)
+            @test unwhiten(S, x) ≈ unwhiten(SM, x)
+            @test unwhiten!(S, copy(x)) ≈ unwhiten(SM, x)
+            @test unwhiten!(r, S, x) ≈ unwhiten(SM, x)
+        end
+        end
     end
-    @testset "Aqua" begin
+    @testset "Code quality (Aqua.jl)" begin
         Aqua.test_all(PSDMatrices)
+    end
+    @testset "Code linting (JET.jl)" begin
+        # @test JET.test_package(
+        #     PSDMatrices;
+        #     target_defined_modules=true,
+        # )
     end
 end
 nothing
